2356d76cac206a804d62bf03401efbcbd266851c
Jamhson Boliva <bolivido@yahoo.com>
2025-08-22 00:10:08 -0400

    🔧 Fix ESLint build error - Remove unused useEffect import, add ESLint warning rules

--- src/components/AudioUploader.js
+++ src/components/AudioUploader.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import config from '../config';
 
 const AudioUploader = ({ onAudioSelect, onAudioRemove, uploadedAudios, serverUrl = config.serverUrl }) => {
